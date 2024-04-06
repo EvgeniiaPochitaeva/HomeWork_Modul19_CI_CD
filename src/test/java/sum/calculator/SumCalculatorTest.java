5b5982b1f1e7037fda4520d50730053a5ca4fc62
Evgeniya <evg.pochitaeva@gmail.com>
2024-04-06 15:55:20 +0300

    Initial commit

---
+++ src/test/java/sum/calculator/SumCalculatorTest.java
@@ -0,0 +1,31 @@
+package sum.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SumCalculatorTest {
+
+    private SumCalculator sumCalculator;
+    @BeforeEach
+    public void beforeEach() {
+        sumCalculator = new SumCalculator();
+    }
+    @Test
+    void testCase1() {
+        Assertions.assertEquals(1, SumCalculator.sum(1));
+    }
+
+    @Test
+    void testCase2() {
+        Assertions.assertEquals(6, SumCalculator.sum(3));
+    }
+
+    @Test
+    void testCaseExceprion(){
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            SumCalculator.sum(0);
+        }, "Input of 0 should throw IllegalArgumentException");
+    }
+
+}
